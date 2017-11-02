4f76af7a0a5f7fd284bf17ee63312589d78cb138
chayann <chayann.bradford@ada.ac.uk>
2017-11-02 18:38:08 +0000

    Added .java class.

---
+++ Oyster.java
@@ -0,0 +1,17 @@
+
+/**
+ * Chayann Bradford
+ */
+public class Oyster
+{
+    boolean eligbleForOyster(boolean livesInLondon, int age){
+
+    if(age >= 16 && age <=19 && livesInLondon){
+           return true;
+        }
+
+    else return false;
+
+    }
+
+}
